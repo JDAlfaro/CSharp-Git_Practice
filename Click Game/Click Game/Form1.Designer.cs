5c70ffd26fd162beb085af73f7b4e05353194b99
JDAlfaro <JoshuaDavidAlfaro@gmail.com>
2013-02-14 12:33:58 -0500

    Added a simple Click Game

---
+++ Click Game/Click Game/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Click_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(215, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLabel.Location = new System.Drawing.Point(259, 9);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(13, 13);
+            this.scoreLabel.TabIndex = 1;
+            this.scoreLabel.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Click Game/Click Game/Form1.cs
@@ -0,0 +1,67 @@
+﻿/* Name: Click Game
+ * Author: Joshua David Alfaro
+ * 
+ * Desciption:
+ * Simple Click Game using a random generator
+ * Followed tutorial by TutorialHouseNz on YouTube
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Click_Game
+{
+    public partial class Form1 : Form
+    {
+        private Rectangle buttonRec = new Rectangle(0, 0, 50, 50);
+        private Random rand = new Random();
+        private int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            drawButton(e.Graphics);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int diffX = e.X - buttonRec.X;
+            int diffY = e.Y - buttonRec.Y;
+
+            if ((diffX > 0 && diffX < 50) && (diffY > 0 && diffY < 50))
+            {
+                score = score + 1;
+                scoreLabel.Text = score.ToString();
+            }
+        }
+
+        private void drawButton(Graphics paper)
+        {
+            buttonRec.X = rand.Next(220);
+            buttonRec.Y = rand.Next(220);
+
+            paper.DrawImage(Click_Game.Properties.Resources.button_sounds_3, buttonRec);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // tells to repaint
+            this.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
